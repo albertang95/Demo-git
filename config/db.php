61ee644058250d73f00217fb36fff9427a25b9d2
Albert <131110526@students.mikroskil.ac.id>
2016-08-24 00:23:46 +0700

    dd

---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=scholardb',
+    'username' => 'root',
+    'password' => '',xss
+    'charset' => 'utf8',
+];
+
+?>
